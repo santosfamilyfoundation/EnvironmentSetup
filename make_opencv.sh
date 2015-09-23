74acbe8f6a65c349a0238fc52b5826248b3a113c
Ryan Eggert <RyanEggertWI@gmail.com>
2015-09-23 11:47:56 -0700

    Updated scripts to fix sourcing .bashrc issue. Updated readme accordingly.

--- make_opencv.sh
+++ make_opencv.sh
@@ -2,24 +2,6 @@ echo "Type the number of cores you have available for building opencv (integer >
 
 read cores;
 
-echo "Installing Dependencies"
-sudo apt-get install -y build-essential cmake git pkg-config;
-sudo apt-get install -y libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev;
-sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev;
-sudo apt-get install -y libgtk2.0-dev;
-sudo apt-get install -y libatlas-base-dev gfortran;
-wget https://bootstrap.pypa.io/get-pip.py;
-sudo python3 get-pip.py;
-sudo pip3 install virtualenv virtualenvwrapper;
-
-# Update ~/.bashrc
-echo "Modifying .bashrc";
-echo "# virtualenv and virtualenvwrapper" >> ~/.bashrc;
-echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc;
-echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc;
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc;
-source ~/.bashrc;
-
 # Make virtualenv
 echo "Creating \"SCOPE\" Virtual Environment";
 mkvirtualenv SCOPE;
