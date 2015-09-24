44df849a7d233d09c75216c0c966937fa2b3fd37
Ryan Eggert <RyanEggertWI@gmail.com>
2015-09-24 07:57:43 -0700

    Looks like you can't create python virtual environments through bash scripts as one might expect. Amended dependencies_opencv.sh and readme.md.

--- dependencies_opencv.sh
+++ dependencies_opencv.sh
@@ -5,8 +5,8 @@ sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install -y libgtk2.0-dev;
 sudo apt-get install -y libatlas-base-dev gfortran;
 wget https://bootstrap.pypa.io/get-pip.py;
-sudo python3 get-pip.py;
-sudo pip3 install virtualenv virtualenvwrapper;
+sudo -H python3 get-pip.py;
+sudo -H pip3 install virtualenv virtualenvwrapper;
 
 # Update ~/.bashrc
 echo "Modifying .bashrc";
